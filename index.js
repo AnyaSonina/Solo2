ee04c37319c867d9c7c9a81ecc1368717e9ab760
AnyaSonina <89742951+AnyaSonina@users.noreply.github.com>
2022-12-21 13:16:35 +0000

    Stretch goals completed

--- index.js
+++ index.js
@@ -5,16 +5,38 @@ const password3 = document.querySelector(".password_el_3")
 const password4 = document.querySelector(".password_el_4")
 const passwordCont = document.getElementsByClassName("password__container")
 const popUp = document.getElementById("pop-up")
+const toggleNums = document.getElementById("toggle1")
+const toggleChars = document.getElementById("toggle2")
+
+
 
 
 const inputEl = document.getElementById("input_el")
 
-let allChars = []
-for(let i=32; i<127; i++) {
-  allChars.push(String.fromCharCode(i))
+let allChars 
+
+function toggling() {
+  allChars = []
+  for(let i=32; i<127; i++) {
+    allChars.push(String.fromCharCode(i))
+  }
+
+  let arrWithoutNums = allChars.filter(i => isNaN(i))
+  let arrWithoutChars = allChars.toString().replace(/[^a-zA-Z0-9 ]/g, '')
+  let arrWithoutCharsAndNums = allChars.toString().replace(/[^a-zA-Z ]/g, '')
+    
+  if(!toggleNums.checked && toggleChars.checked){
+    allChars = arrWithoutNums
+  }else if(!toggleChars.checked && toggleNums.checked){   
+    allChars = Array.from(arrWithoutChars)
+  }else if(!toggleNums.checked && !toggleChars.checked) {   
+      allChars = Array.from(arrWithoutCharsAndNums)
+  }
+ 
 }
 
 function passwordGenerate(){
+  toggling() 
   let generatedPassword = ""
   for(let i=0; i<inputEl.value; i++) {
     generatedPassword += allChars[Math.floor(Math.random() * allChars.length)]
@@ -22,6 +44,7 @@ function passwordGenerate(){
   return generatedPassword
 
 }
+
 
 function render(){
   password1.textContent=passwordGenerate()
@@ -34,14 +57,16 @@ btnGenerator.addEventListener("click",render)
 
 for(let i = 0; i<passwordCont.length; i++) {
   passwordCont[i].addEventListener("click", (event) => {
-    if(event.target.matches(".password__container")){
-      navigator.clipboard.writeText(event.target.textContent)
-    }    
-    popUp.innerHTML = `${event.target.innerText}  <span style="color:#1F2937;">copied to clipboard</span> `
-    popUp.style.opacity = 1
-    timeoutId = setTimeout(() => {
-      popUp.style.opacity = 0      
-    }, 1000)
+    if(passwordCont[i].childNodes.length > 0) {
+      if(event.target.matches(".password__container")){
+        navigator.clipboard.writeText(event.target.textContent)
+      }    
+      popUp.innerHTML = `${event.target.innerText}  <span style="color:#1F2937;">copied to clipboard</span> `
+      popUp.style.opacity = 1
+      let timeoutId = setTimeout(() => {
+        popUp.style.opacity = 0      
+      }, 1000)
+    }
     
   })
 }
